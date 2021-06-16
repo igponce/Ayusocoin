d29b6a3a246d17da404eca76585c1f354d1bb2aa
Inigo <igponce@gmail.com>
2021-06-16 17:56:57 +0200

    Despliega contratos y envia los token a las direcciones de origen
    Estas direcciones de origen sin las de personas relevantes
    del mundo de la política. Ellos no tienen ningún wallet ethereum
    y seguramente no saben qué es esto así que les mandaremos un
    paper wallet para que lo puedan ver.

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,23 +2,37 @@
 
 from brownie import Ayusocoin, Faucet, accounts
 
-token = accounts[0].deploy(Ayusocoin)
-faucet = accounts[0].deploy(Faucet,token.address, 100_000000)
 
-alltokens = token.totalSupply()
+def DeployContracts():
+    
+    token = accounts[0].deploy(Ayusocoin)
+    faucet = accounts[0].deploy(Faucet,token.address, 100_000000)
+    
+    root = accounts[0].address
+    
+    alltokens = token.totalSupply()
+    
+    # Direcciones iniciales de envío
+    
+    addresses = [
+      '0x0E1A67bC08aE03E5F9FE9857e575C702326Df72E',
+      '0xdCd2F6210000F5c1c1e97BDB08F2c8E0879aABb5',
+      '0xa03af4993912C6b8c48Cb3AB234D7101A514714E'
+    ]
+    
+    # Enviamos tokens iniciales a las paper wallet que
+    for addr in addresses:
+      token.transfer(addr, 1000_000, {'from': accounts[0].address})
+    
+    balance_restante = token.balanceOf(root)
+    token.setMaxBalancePerAddress(alltokens, {"from": root})
+    
+    token.transfer(faucet.address, balance_restante, {'from': root })
+    token.setMaxBalancePerAddress(10_000_000000, {"from": root})
 
-# Direcciones iniciales de envío
+    print(f"Token - address: {token.address}\nFaucet - address: {faucet.address}")
 
-addresses = [
-  '0x0E1A67bC08aE03E5F9FE9857e575C702326Df72E',
-  '0xdCd2F6210000F5c1c1e97BDB08F2c8E0879aABb5',
-  '0xa03af4993912C6b8c48Cb3AB234D7101A514714E'
-]
+def main():
 
-# Enviamos tokens iniciales a las paper wallet que
-for addr in addresses:
-  token.transfer(addr, 1000_000, {'from': accounts[0].address})
-
-balance_restante = token.getBalance({'from': accounts[0].address})
-token.transfer(faucet.address, balance_restante, {'from': accounts[0].address})
-token.setMaxBalancePerAddress(cantidad_max, {"from": root})
+    print("Desplegando contratos")
+    DeployContracts()
