b8742969136dea6ee45acc04720a549af4c11c18
Inigo <igponce@gmail.com>
2021-06-26 23:38:42 +0200

    Corregido el valor inicial en el faucet: 1000 tokens

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -6,7 +6,7 @@ from brownie import Ayusocoin, Faucet, accounts
 def DeployContracts():
     
     token = accounts[0].deploy(Ayusocoin)
-    faucet = accounts[0].deploy(Faucet,token.address, 100_000000)
+    faucet = accounts[0].deploy(Faucet,token.address, 1000_000000)
     
     root = accounts[0].address
     
@@ -23,7 +23,7 @@ def DeployContracts():
     
     # Enviamos tokens iniciales a las paper wallet que
     for addr in addresses:
-      token.transfer(addr, 1_123456, {'from': accounts[0].address})
+      token.transfer(addr, 12345, {'from': accounts[0].address})
     
     balance_restante = token.balanceOf(root)
     token.setMaxBalancePerAddress(alltokens, {"from": root})
